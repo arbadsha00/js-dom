1e33ad4b0b3a131be15de06bd7c1dc75e5b10810
arbadsha00 <arbadsha00@gmail.com>
2024-09-19 09:19:45 +0600

    remove and clear button done

--- dom.js
+++ dom.js
@@ -15,19 +15,42 @@ moodBtn.addEventListener("click", function () {
 
 // todo -list
 let count = 1;
+
 document.getElementById("add-button").addEventListener("click", function () {
   const listItem = document.getElementById("text-area").value;
   document.getElementById("text-area").value = "";
 
   const listContainer = document.getElementById("list-container");
- 
+
   const row = document.createElement("tr");
-  row.innerHTML = `
-              <th>${count++}</th>
-              <td>${listItem}</td>
-              <td><button class="btn cross-btn">X</button></td>
-            `;
 
-  listContainer.append(row);
+  if (listItem == "") {
+    alert("Write Something");
+  } else {
+    row.innerHTML = `
+    <th class="variable">${count++}</th>
+    <td>${listItem}</td>
+    <td><button onclick="remove(event)"  class="btn cross-btn">X</button></td>
+  `;
+    listContainer.append(row);
+  }
 });
+function remove(event) {
 
+
+  const child = event.target.parentNode.parentNode;
+  const arr = Array.from(child.parentNode.children);
+  for (let i = arr.indexOf(child) + 1; i < arr.length; i++) {
+    const childUpdate = child.parentNode.children[i];
+    childUpdate.getElementsByTagName("th")[0].innerText -= 1;
+    
+  }
+  event.target.parentNode.parentNode.remove();
+  count--;
+}
+
+document.getElementById("clear-btn").addEventListener("click",function () {
+  count = 1;
+  document.getElementById("list-container").innerHTML = "";
+  console.log("ok");
+})
